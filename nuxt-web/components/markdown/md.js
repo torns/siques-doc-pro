10592d40cc1a7e73e7886fb21c8fb69a05d5ec3b
ericheshenghao <943452349@qq.com>
2020-02-14 01:00:32 +0800

    markdown编辑器

--- nuxt-web/components/markdown/md.js
+++ nuxt-web/components/markdown/md.js
@@ -4,10 +4,23 @@ export default (height) => {
   // eslint-disable-next-line
   return new tui.Editor({
     /* eslint-disable */
-    el: document.getElementById('editorSection'),
+    el: document.querySelector('#editorSection'),
     initialEditType: 'markdown',
     language: 'zh_CN',
+    placeholder: '来愉快的造作吧',
     height: height ? height : '700px',
+    exts: [
+      {
+        name: 'chart',
+        minWidth: 100,
+        maxWidth: 600,
+        minHeight: 100,
+        maxHeight: 300
+      },
+      'scrollSync',
+      'colorSyntax'
+    ],
+
     ...defalutConfig
   })
 }
