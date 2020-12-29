3c35ad82df1020f154fdb8816a83e05c66b66c89
ericheshenghao <943452349@qq.com>
2020-12-29 13:58:16 +0800

    diff改为word比较

--- nuxt-web/plugins/diff.js
+++ nuxt-web/plugins/diff.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
 /** nuxt.config.js  https://cdn.bootcdn.net/ajax/libs/jsdiff/5.0.0/diff.js*/
 export default (oldStr, newStr) => {
-  return Diff.diffChars(oldStr, newStr)
+  return Diff.diffWordsWithSpace(oldStr, newStr)
 }
