b5a1792eb9af9154e006298d0e121c5982f1f5df
何胜豪 <heshengh@bsoft.com.cn>
2021-07-18 20:56:25 +0800

    bug修复

--- nuxt-web/components/Base/BaseLoading/index.vue
+++ nuxt-web/components/Base/BaseLoading/index.vue
@@ -2,7 +2,10 @@
   <v-dialog v-model="loadMsg.loading" hide-overlay persistent width="300">
     <v-card color="primary" dark>
       <v-card-text>
-        {{ loadMsg.text || `上传中` }}
+        <div class="d-flex jc-between">
+          <div>{{ loadMsg.text || `上传中` }}</div>
+          <v-icon @click="close" small>mdi-close</v-icon>
+        </div>
         <v-progress-linear indeterminate color="white" class="mb-0"></v-progress-linear>
       </v-card-text>
     </v-card>
@@ -17,6 +20,10 @@ import { mapGetters } from 'vuex'
 })
 export default class BaseLoading extends Vue {
   loadMsg
+
+  close() {
+    this.$store.commit('modules/tips/STOP_LOADING')
+  }
 }
 </script>
 
